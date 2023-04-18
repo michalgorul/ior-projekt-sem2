c6ba118fbb0fc0bd98be3336bdf1856a3d0e88c5
michalgorul <michalgorul@gmail.com>
2023-04-18 19:05:43 +0200

    first commit

---
+++ src/main/java/pl/polsl/aei/ior/springdata/SpringDataApplication.java
@@ -0,0 +1,13 @@
+package pl.polsl.aei.ior.springdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataApplication.class, args);
+	}
+
+}
